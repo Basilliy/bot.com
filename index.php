48286b7b087f785dc5bd4f5c142dae09d84282bf
russik2013 <z.kon2009@yandex.ru>
2016-07-12 12:01:53 +0300

    ru

--- index.php
+++ index.php
@@ -8,11 +8,12 @@ $url  = "https://api.telegram.org/bot".$token;
 //$result = json_decode($_POST, TRUE);
 //$id = $_SERVER["result"][0]["message"]["chat"]["id"];
 
-print_r("post = ".$_POST);
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
 
-//print_r("res=".$result);
+print_r("res=".$output);
 //print_r("/n");
-//print_r("id = ".$id);
+print_r("id = ".$chat_id);
 //echo curl_version();
 //////////ВАЖНО /////
 $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
@@ -20,3 +21,4 @@ $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
 $update = file_get_contents($url."/sendmessage?chat_id=267280685&text=$fuck");
 
 
+//https://api.telegram.org/bot246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA/setWebhook?url=https://marvinbots.herokuapp.com/index.php
